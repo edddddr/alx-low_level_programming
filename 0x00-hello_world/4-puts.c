1b81924717aad4b50486a6742a72c2f9fdf66155
edddddr <endt6342@gmail.com>
2022-09-13 22:21:22 -0700

    coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
 /**
-*
 *main - print the string in thaput function
 *this program prints "Programming is like building a multilingual puzzle
-*Return: 0
+*Return: 0 
 */
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+puts("\"Programming is like building a multilingual puzzle"); 
+return (0);
 }
