679aa14efb3fe47b3cc23a4833222b2c0b422bc0
edddddr <endt6342@gmail.com>
2022-09-15 11:52:42 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
+
+int_putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int print_sign(int n);
+int -abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void timesd_table(void);
+int add(int a, int b);
+void print_to_98(int n);
+void timesd_table(int n);
+void num(int r, int n);
+
+#endif
