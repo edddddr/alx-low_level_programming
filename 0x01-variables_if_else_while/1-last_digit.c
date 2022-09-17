d1a51c2fc2a53b86f39bc34166d95c197da82688
endrias <97146464+edddddr@users.noreply.github.com>
2022-09-16 17:59:28 -0700

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,18 +8,16 @@
 * this program prints "Programming is positive, zero, or negative
 * Return: 0
 */
-
 int main(void)
 {
 int n;
 int l;
-	
+
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 l = n % 10;
-	
+
 if (l > 5)
-	
 {
 	printf("Last digit of %d is %d and is greater than 5\n", n, l);
 }
@@ -32,3 +30,4 @@ else
 	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
 }
 return (0);
+}
