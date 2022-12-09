7404bf8f1e62803ba5f605f3df1b67404e6e636b
endrias <97146464+edddddr@users.noreply.github.com>
2022-12-09 15:43:32 -0800

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,55 +1,18 @@
 #include <stdio.h>
 
 /**
-
- * main - print if the number is postive, zero, or negative
-<<<<<<< HEAD
-
- *
-
-=======
- *
->>>>>>> 867fc4d43ad2b11cecd24505c56c5a4042e2a7da
- * Description: using the main function
-
- * this program prints "Programming is positive, zero, or negative
-<<<<<<< HEAD
-
- * Return: 0
-
-=======
- * Return: 0
->>>>>>> 867fc4d43ad2b11cecd24505c56c5a4042e2a7da
- */
-
+* main - Enter pint
+* REturn always 0 (success)
+*/
 int main(void)
-
 {
-
-char ch;
-
-for (ch = 'a' ; ch <= 'z' ; ch++)
-
-{
-<<<<<<< HEAD
-
-if (ch != 'e' && ch != 'q')
-
-=======
-	putchar(ch);
-}
-for (ch = 'A' ; ch <= 'Z' ; ch++)
->>>>>>> 867fc4d43ad2b11cecd24505c56c5a4042e2a7da
-{
-
-	putchar(ch);
-
-}
-
-}
-
-putchar('\n');
-
-return (0);
-
+	char c;
+	
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != '0' && c != 'q')
+			putchar(c);
+	}
+	putchar('\n');
+	return (0);
 }
