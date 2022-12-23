9823491a4f50fe0a748098329d3ed14bcd5f3899
endrias <97146464+edddddr@users.noreply.github.com>
2022-12-22 18:34:57 -0800

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -5,27 +5,31 @@
  * @n: input integer.
  * Return: no return.
  */
-void printnumber(int n)
+void print_number(int n)
 {
-    if(i/10!=0)
-    {
-        printnumber(i/10);
-        if ( n > 0 )
-        {
-           putchar(n%10 + '0');
-        }
-        else
-        {
-           putchar(-n%10 + '0');
-        }
-    }
-    else if((n/10==0) && (n%10!=0) && (n>0))
-    {
-        putchar(n%10 + '0');
-    }
-    else if((i/10==0) && (i%10!=0) && (i<=0))
-    {
-        putchar('-');
-        putchar(-n%10+'0');
-    }    
+	unsigned int i, l, count;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		i = n * -1;
+	}
+	else
+	{
+		i = n;
+	}
+
+	l = i;
+	count = 1;
+
+	while (l > 9)
+	{
+		l /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((i / count) % 10) + 48);
+	}
 }
