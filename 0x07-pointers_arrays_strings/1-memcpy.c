9368a8fcc9fc81058fcf380fcfbb11b8f1ec7f5b
endrias <97146464+edddddr@users.noreply.github.com>
2023-01-02 10:21:29 -0800

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,19 +1,17 @@
-#include "main.h"                                                                                                                       
-                                                                                                                                        
-/**                                                                                                                                     
- * *_memcpy -  copies memory area from src to dest                                                                                      
- * @dest: destination                                                                                                                   
- * @src: source                                                                                                                         
- * @n: max bytes to use                                                                                                                 
- * Return: dest                                                                                                                         
- */                                                                                                                               
-char *_memcpy(char *dest, char *src, unsigned int n)                                                                                    
-{                                                                                                                                       
-        unsigned int i;                                                                                                                 
-                                                                                                                                        
-        for (i = 0; n > 0 ; i++, n--)                                                                                                   
-        {                                                                                                                               
-                dest[i] = src[i];                                                                                                       
-        }                                                                                                                       
-        return (dest);                                                                                                                  
+#include "main.h"
+
+/**
+* _memcpy - function to copy memory
+* @dest: destination memory
+* @src: source memory
+* @n: number of bytes to be copied
+* Return: string copied from source
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+return (dest);
 }
