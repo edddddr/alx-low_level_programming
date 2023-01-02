68eb47b338190cf59f3902d09a7fc619032b7f14
endrias <97146464+edddddr@users.noreply.github.com>
2023-01-02 10:20:53 -0800

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -6,9 +6,7 @@
  * @src: source                                                                                                                         
  * @n: max bytes to use                                                                                                                 
  * Return: dest                                                                                                                         
- */                                                                                                                                     
-                                                                                                                                        
-                                                                                                                                        
+ */                                                                                                                               
 char *_memcpy(char *dest, char *src, unsigned int n)                                                                                    
 {                                                                                                                                       
         unsigned int i;                                                                                                                 
