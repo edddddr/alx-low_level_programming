92049d9944884afc96329ad4d6563f8cfcd4bc0a
endrias <97146464+edddddr@users.noreply.github.com>
2023-01-02 09:01:48 -0800

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,45 +1,23 @@
 #include "main.h"
 
-
-
 /**
-
  * _strchr - prints found c
-
  * @s: pointer to char
-
  * @c: a parameter that we found
-
  * Return: *S
-
 */
 
-
-
 char *_strchr(char *s, char c)
-
 {
-
 	while (*s)
-
 	{
-
 		if (*s != c)
-
 			s++;
-
 		else
-
 			return (s);
-
 	}
-
 	if (c == '\0')
-
 		return (s);
 
-
-
 	return (NULL);
-
-} 
+}
