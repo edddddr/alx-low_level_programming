def600ae9ec1c509f328438bb362f3e9ac546d78
endrias <97146464+edddddr@users.noreply.github.com>
2023-01-04 11:42:32 -0800

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,12 +1,16 @@
-#include <unistd.h>           
+#include "main.h"
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Prints a string in reverse.
+ * @s: The string  will printed.
+ * Return: No.
  */
-int _putchar(char c)
+void _puts_recursion(char *s)
 {
-	return (write(1, &c, 1));
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
