ec27f9b22302f4da32b72ecfd39b9048baea9e9e
endrias <97146464+edddddr@users.noreply.github.com>
2023-01-07 03:19:48 -0800

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,55 +1,28 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 /**
+ * main - multiplies two numbers
+ * @argc: number of argument passed
+ * @argv: array of arguments
  *
- *  * main - multiplies two numbers
- *
- *   * @argc: number of argument passed
- *
- *    * @argv: array of arguments
- *
- *     *
- *
- *      * Return: returns nothing
- *
- *       */
-
-
+ * Return: returns nothing
+ */
 
 int main(int __attribute__((__unused__)) argc, char *argv[])
-
 {
+	int x, y;
 
-		int x, y;
-
-
-
-			if (argc <= 2)
-
-					{
-
-								printf("%s\n", "Error");
-
-										return (1);
-
-											}
+	if (argc <= 2)
+	{
+		printf("%s\n", "Error");
+		return (1);
+	}
 
 
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
 
+	printf("%d\n", x * y);
 
-
-				x = atoi(argv[1]);
-
-					y = atoi(argv[2]);
-
-
-
-						printf("%d\n", x * y);
-
-
-
-							return (0);
-
+	return (0);
 }
