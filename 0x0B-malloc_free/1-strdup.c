9ad06b435d777ff36bb05b1853192896b7428d57
endrias <97146464+edddddr@users.noreply.github.com>
2023-01-10 12:13:04 -0800

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,53 +1,27 @@
-#include <main.h>
-
+#include <stdlib.h>
 /**
+ * *_strdup - creates an array of chars,
+ * and initializes it with a specific char
+ * @str: elements  of the array to create
  *
- *  * *_strdup - creates an array of chars,
- *
- *   * and initializes it with a specific char
- *
- *    * @str: elements  of the array to create
- *
- *     *
- *
- *      * Return: pointer to the array (Success), NULL (Error)
- *
- *       */
-
+ * Return: pointer to the array (Success), NULL (Error)
+ */
 char *_strdup(char *str)
-
 {
+char *dup;
+unsigned int i, len;
+i = 0;
+len = 0;
+if (str == NULL)
+return (NULL);
 
-	char *dup;
+while (str[len])
+len++;
+dup = malloc(sizeof(char) * (len + 1));
+if (dup == NULL)
+return (NULL);
+while ((dup[i] = str[i]) != '\0')
+i++;
 
-	unsigned int i, len;
-
-	i = 0;
-
-	len = 0;
-
-	if (str == NULL)
-
-		return (NULL);
-
-
-
-	while (str[len])
-
-		len++;
-
-	dup = malloc(sizeof(char) * (len + 1));
-
-	if (dup == NULL)
-
-		return (NULL);
-
-	while ((dup[i] = str[i]) != '\0')
-
-		i++;
-
-
-
-	return (dup);
-
+return (dup);
 }
