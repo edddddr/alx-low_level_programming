63050ca7ae7ad9cc3cf37a599b024069175603c5
endrias <97146464+edddddr@users.noreply.github.com>
2023-01-10 12:17:07 -0800

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,27 +1,29 @@
+#include "main.h"
 #include <stdlib.h>
 /**
- * *_strdup - creates an array of chars,
- * and initializes it with a specific char
- * @str: elements  of the array to create
+ * _strdup - allocate the space in the memory
+ * @str: string
  *
- * Return: pointer to the array (Success), NULL (Error)
+ * Return: put the pointer return value
  */
 char *_strdup(char *str)
 {
-char *dup;
-unsigned int i, len;
-i = 0;
-len = 0;
-if (str == NULL)
-return (NULL);
+	char *strout;
+	unsigned int u, s;
 
-while (str[len])
-len++;
-dup = malloc(sizeof(char) * (len + 1));
-if (dup == NULL)
-return (NULL);
-while ((dup[i] = str[i]) != '\0')
-i++;
+	if (str == NULL)
+		return (NULL);
 
-return (dup);
+	for (i = 0; str[u] != '\0'; u++)
+		;
+
+	strout = (char *)malloc(sizeof(char) * (u + 1));
+
+	if (strout == NULL)
+		return (NULL);
+
+	for (s = 0; s <= u; s++)
+		strout[s] = str[s];
+
+	return (strout);
 }
