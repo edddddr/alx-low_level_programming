3757a7d6542878339b4f40d14f38ae0478ddb119
endrias <97146464+edddddr@users.noreply.github.com>
2023-01-16 11:31:05 -0800

    Update dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,65 +1,32 @@
-#ifndef _DOG_H_
-
-#define _DOG_H_
-
-
-
-#include <stdio.h>
-
-#include <stdlib.h>
-
+#ifndef DOG_H
+#define DOG_H
 
 
 /**
- *
- *  * dog_t - Typedef for struct dog
- *
- *   */
-
-typedef struct dog dog_t;
-
-
-
-/**
- *
- *  * struct dog - struct that stores some of a dog
- *
- *   * @name: dog name
- *
- *    * @age: dog age
- *
- *     * @owner: owner of the dog
- *
- *      *
- *
- *       * Description: struct called "dog" that stores its name, its age
- *
- *        * and the name of its owner.
- *
- *         */
-
+* struct dog - a dog's basic info
+* @name: First member
+* @age: Second member
+* @owner: Third member
+*
+* Description: Longer description
+*/
 struct dog
-
 {
-
-		char *name;
-
-			float age;
-
-				char *owner;
-
+char *name;
+float age;
+char *owner;
 };
 
-
+/**
+* dog_t - typedef for struct dog
+*/
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
 void print_dog(struct dog *d);
-
 dog_t *new_dog(char *name, float age, char *owner);
-
 void free_dog(dog_t *d);
-
-
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 
 #endif
