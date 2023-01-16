860b357991bd28b3d08fbc861becfc05419e423a
endrias <97146464+edddddr@users.noreply.github.com>
2023-01-16 11:18:45 -0800

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,45 +1,33 @@
-1-init_dog.c CODE
+#ifndef _DOG_H_
+#define _DOG_H_
 
-
-
-
-
+#include <stdio.h>
 #include <stdlib.h>
 
-	#include "dog.h"
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
 
+/**
+ * struct dog - struct that stores some of a dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: owner of the dog
+ *
+ * Description: struct called "dog" that stores its name, its age
+ * and the name of its owner.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
-
-
-	/**
-	 *
-	 * 	 * init_dog - initializes a variable of type struct dog
-	 *
-	 * 	 	 * @d: pointer to struct dog to initialize
-	 *
-	 * 	 	 	 * @name: name to initialize
-	 *
-	 * 	 	 	 	 * @age: age to initialize
-	 *
-	 * 	 	 	 	 	 * @owner: owner to initialize
-	 *
-	 * 	 	 	 	 	 	 */
-
-	void init_dog(struct dog *d, char *name, float age, char *owner)
-
-		{
-
-					if (d == NULL)
-
-									d = malloc(sizeof(struct dog));
-
-							d->name = name;
-
-									d->age = age;
-
-											d->owner = owner;
-
-												}
-
-
+#endif
