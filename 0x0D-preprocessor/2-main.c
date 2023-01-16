5fdb815347b30b3d2579f698ca0fa28853159857
endrias <97146464+edddddr@users.noreply.github.com>
2023-01-16 12:35:51 -0800

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
- *
- *  * main - prints the name of the file
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+*
+*  * main - prints the name of the file
+*   *
+*    * Return: Always 0 (Success)
+*
+*     
+*/
 
 int ain(void)
 
@@ -18,4 +17,3 @@ printf("%s\n", __FILE__);
 
 return (0);
 }
-
